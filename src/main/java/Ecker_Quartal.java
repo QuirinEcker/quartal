eca734fb47e373eddaf4a02dee499e5eb2993a67
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-28 12:34:57 +0200

    code tested

--- src/main/java/Ecker_Quartal.java
+++ src/main/java/Ecker_Quartal.java
@@ -8,10 +8,10 @@ public class Ecker_Quartal {
         String name;
         int bornmonth;
 
-        System.out.print("Geben sie Ihren Vornamen ein              :");
+        System.out.print("Geben sie Ihren Vornamen ein            : ");
         name = scanner.next();
 
-        System.out.println("Geben sie Ihr Geburtsmonat als Zahl ein :");
+        System.out.print("Geben sie Ihr Geburtsmonat als Zahl ein : ");
         bornmonth = scanner.nextInt();
 
         if (bornmonth <= 3){
@@ -22,12 +22,16 @@ public class Ecker_Quartal {
             System.out.println(name + ", du hast im 2. Quartel Geburtstag - Happy Birthday");
         }
 
-        if (bornmonth >= 7 && bornmonth < 9){
+        if (bornmonth >= 7 && bornmonth <= 9){
             System.out.println(name + ", du hast im 3. Quartel Geburtstag - Happy Birthday");
         }
 
-        if (bornmonth <= 12){
+        if (bornmonth <= 12 && bornmonth > 9){
             System.out.println(name + ", du hast im 4. Quartel Geburtstag - Happy Birthday");
+        }
+
+        else {
+            System.out.println("Falsche Eingabe");
         }
     }
 }
